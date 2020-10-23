a315cbef371ecb68930cf40ddbca361b6b367f09
pedrocarrijo <pedro.carrijo@oracle.com>
2020-10-23 09:29:30 -0300

    err

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const http = require('http');
 const server = http.createServer();
 
 server.on('request', function (request, response) {
-  var name = "NomePessoa"
-  response.write('Your random name: ' + nome);
+  var name = "NomePessoa";
+  setNome("Pedro");
+  response.write('Seu nome: ' + nome);
   response.end();
 });
 
 server.listen(3000);
+
+function setNome(var nome){
+	name = nome;
+}
 console.log('Server is running.');

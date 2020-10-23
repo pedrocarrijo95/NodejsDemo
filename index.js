0552524384b8eeb96b18b278282bb6123c6b4f91
pedrocarrijo <pedro.carrijo@oracle.com>
2020-10-23 09:08:32 -0300

    f

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServera();
 
 server.on('request', function (request, response) {
   const randomName = "random";
